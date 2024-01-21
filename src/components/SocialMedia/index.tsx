668fc1397942f04abe0c2a65cfaacb7d9f01cd57
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-01-21 20:26:44 +0100

    Create SocialMedia component for icons

---
+++ src/components/SocialMedia/index.tsx
@@ -0,0 +1,40 @@
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import './styles.scss';
+
+interface SocialMediaProps {
+  mediaName: string;
+  mediaLink: string;
+  mediaIconColor: string;
+}
+
+function SocialMedia({ mediaName, mediaLink, mediaIconColor }: SocialMediaProps) {
+  const socialMediaName = `social-media_${mediaName}`;
+  const iconStyle = { fontSize: '24px', color: 'inherit' };
+
+  // Icone en fonction du nom du media
+  const socialMediaIcon = (media: string) => {
+    switch (media) {
+      case 'linkedin':
+        return <AiFillLinkedin style={iconStyle} />;
+      case 'github':
+        return <AiFillGithub style={iconStyle} />;
+      default:
+        return null;
+    }
+  };
+  return (
+    <a
+      href={mediaLink}
+      target="_blank"
+      rel="noreferrer"
+      className={`social-media__link link-color_${mediaIconColor}`}
+    >
+      <div className={`social-media-icon ${socialMediaName}`}>
+        {socialMediaIcon(mediaName)}
+
+      </div>
+    </a>
+  );
+}
+
+export default SocialMedia;
