5414d963152d9d6891c3c3fcd943ce10381da640
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-01-21 16:34:36 +0100

    Footer - add portfolio logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,14 @@
+import portfolioLogo from '../../assets/images/portfolio-logo.png';
 import './styles.scss';
 
 function Footer() {
   return (
-    <footer className="footer" />
+    <footer className="footer">
+      <div className="footer__logo">
+        <img className="footer__logo-portfolio" src={portfolioLogo} alt="logo du portfolio avec initiales" />
+        <div className="footer__logo-social-media" />
+      </div>
+    </footer>
 
   );
 }
