dcf152e1cb6a1dbc7c463d9b24851fa7d3fbf9c5
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-01-21 20:29:02 +0100

    Footer: add SocialMedia icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,5 @@
 import portfolioLogo from '../../assets/images/portfolio-logo.png';
+import SocialMedia from '../SocialMedia';
 import './styles.scss';
 
 function Footer() {
@@ -6,7 +7,10 @@ function Footer() {
     <footer className="footer">
       <div className="footer__logo">
         <img className="footer__logo-portfolio" src={portfolioLogo} alt="logo du portfolio avec initiales" />
-        <div className="footer__logo-social-media" />
+        <div className="footer__logo-social-media">
+          <SocialMedia mediaName="linkedin" mediaLink="#" mediaIconColor="white" />
+          <SocialMedia mediaName="github" mediaLink="#" mediaIconColor="white" />
+        </div>
       </div>
     </footer>
 
