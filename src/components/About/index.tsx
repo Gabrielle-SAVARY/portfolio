a86d758ba83c3c32794f895ef40e49cfb7433b26
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-01-24 18:03:14 +0100

    About: add layout

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,4 +1,7 @@
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import CV from '../../assets/CV PORTFOLIO développeur web .pdf';
 import portraitImage from '../../assets/images/image-portrait.png';
+
 import './styles.scss';
 
 function About() {
@@ -8,6 +11,42 @@ function About() {
       <div className="about">
         <div className="about__image">
           <img className="about__image-portrait" src={portraitImage} alt="portrait de profil version animée d'une fille avec un caque audio" />
+        </div>
+        <div className="about__container">
+          <p className="about__container__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nemo illo nulla ullam eaque rem, aspernatur iure incidunt a eum voluptatum architecto corporis, atque officiis aliquam adipisci fugiat soluta quas!</p>
+          <div className="about__container__links">
+            <a
+              href={CV}
+              className="link link--primary typography--link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Télécharger mon CV
+            </a>
+
+            <a
+              href="https://github.com/Gabrielle-SAVARY?tab=repositories"
+              className="link link--secondary typography--link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              mon github
+              {' '}
+              <AiFillGithub style={{ fontSize: '26px', color: 'inherit' }} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/gabrielle-savary/"
+              className="link link--secondary typography--link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              me contacter
+              {' '}
+              <AiFillLinkedin style={{ fontSize: '26px', color: 'inherit' }} />
+            </a>
+
+          </div>
+
         </div>
       </div>
 
