02cb80f2a7492cbac05e6395937b36d2a44603ed
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-01-31 20:22:13 +0100

    Portfolio: add MyProjects layout and component

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,10 +1,14 @@
+import MyProjects from '../MyProjects';
 import './styles.scss';
 
 function Portfolio() {
   return (
     <section id="sectionPortfolio" className="sectionTwo">
+      <h2>MES PROJETS</h2>
       <div className="portfolio__container">
-        <p>MES PROJETS</p>
+        <MyProjects title="Projet 1" backgroundHero="" />
+        <MyProjects title="Projet 2" backgroundHero="" />
+        <MyProjects title="Projet 3" backgroundHero="" />
       </div>
 
     </section>
