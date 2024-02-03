b3388e5f5f731c392be3d85728af4168dbaa55dc
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-02-03 22:42:07 +0100

    MyProjectModal: create modal component - test show modal

---
+++ src/components/MyProjectModal/index.tsx
@@ -0,0 +1,14 @@
+import './styles.scss';
+
+function MyProjectModal() {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h4>test modale</h4>
+      </div>
+    </div>
+
+  );
+}
+
+export default MyProjectModal;
