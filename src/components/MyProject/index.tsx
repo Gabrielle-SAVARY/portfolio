bba67488d1dfaaec65b9333ff9c6c181bd52fc3e
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-02-06 16:52:31 +0100

    MyProject: delete modal component MyProjectModal

--- src/components/MyProject/index.tsx
+++ src/components/MyProject/index.tsx
@@ -5,20 +5,13 @@ interface MyProjectProps {
   name: string;
   legend: string;
   projectImage: string;
-  setIsProjectModalOpen: (value: boolean) => void;
-  setProjectIdModalOpen: (value: number) => void;
 }
 
 function MyProject({
-  id, name, legend, projectImage, setIsProjectModalOpen, setProjectIdModalOpen,
+  id, name, legend, projectImage,
 }: MyProjectProps) {
-  const handleMOpenModal = (event: React.MouseEvent<HTMLButtonElement>, projectId: number) => {
-    setIsProjectModalOpen(true);
-    setProjectIdModalOpen(projectId);
-  };
-
   return (
-    <article className="card">
+    <article className="card" id={`${id}-${name}`}>
       <div className="card__header">
         <img className="card__header__image" src={projectImage} alt={name} />
       </div>
@@ -27,7 +20,7 @@ function MyProject({
         <p className="card__body__project-legend">{legend}</p>
       </div>
       <div className="card__footer">
-        <button type="button" className="card__footer__btn-open-modal" onClick={(event) => handleMOpenModal(event, id)}>En savoir plus</button>
+        <button type="button" className="card__footer__btn">btn</button>
       </div>
     </article>
   );
--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import MyProject from '../MyProject';
-import MyProjectModal from '../MyProjectModal';
 import { IMyProject } from '../../@types/dataMyProject';
 import './styles.scss';
 
@@ -9,12 +8,6 @@ import './styles.scss';
 function Portfolio() {
   // Liste des projets
   const [projectsList, setProjectsList] = useState<IMyProject[]>([]);
-  // Vérifie si la modale du projet est ouverte
-  const [isProjectModalOpen, setIsProjectModalOpen] = useState<boolean>(false);
-  // Id du projet à afficher dans la modale
-  const [projectIdModalOpen, setProjectIdModalOpen] = useState<number>(0);
-
-  const [projectDetailModal, setProjectDetailModal] = useState<IMyProject | null>(null);
 
   const getAllProjects = async () => {
     try {
@@ -27,18 +20,9 @@ function Portfolio() {
     }
   };
 
-  const handleFilterProjectModal = (allProjects: IMyProject[], projectId: number) => {
-    const projectFiltered = allProjects.filter((project) => project.id === projectId);
-    setProjectDetailModal(projectFiltered[0]);
-  };
-
   useEffect(() => {
     getAllProjects();
   }, []);
-
-  useEffect(() => {
-    if (projectIdModalOpen !== 0) handleFilterProjectModal(projectsList, projectIdModalOpen);
-  }, [projectIdModalOpen, projectsList]);
 
   return (
     <section id="sectionPortfolio" className="sectionTwo">
@@ -51,8 +35,6 @@ function Portfolio() {
             name={project.name}
             legend={project.legend}
             projectImage={project.imageLink}
-            setIsProjectModalOpen={setIsProjectModalOpen}
-            setProjectIdModalOpen={setProjectIdModalOpen}
           />
         ))}
 
@@ -61,18 +43,8 @@ function Portfolio() {
           name="Projet 3"
           legend=""
           projectImage=""
-          setIsProjectModalOpen={setIsProjectModalOpen}
-          setProjectIdModalOpen={setProjectIdModalOpen}
         />
-
-        {isProjectModalOpen && (
-        <MyProjectModal
-          setIsProjectModalOpen={setIsProjectModalOpen}
-          projectDetails={projectDetailModal}
-        />
-        )}
       </div>
-
     </section>
   );
 }
