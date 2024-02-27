650aacd5a56b606f4ea6cf90ebd36c7524428021
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-02-27 17:15:23 +0100

    Footer: update logo image and size

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,15 @@
-import portfolioLogo from '../../assets/images/portfolio-logo.png';
+import portfolioLogo from '../../assets/images/Logo-portfolio_white.png';
 import SocialMedia from '../SocialMedia';
 import './styles.scss';
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__logo">
+      <div className="footer__container">
         <a href="/">
-          <img className="footer__logo-portfolio" src={portfolioLogo} alt="logo du portfolio avec initiales" />
+          <img className="footer__container__logo" src={portfolioLogo} alt="logo du portfolio avec initiales" />
         </a>
-        <div className="footer__logo-social-media">
+        <div className="footer__container__ocial-media">
           <SocialMedia mediaName="linkedin" mediaLink="https://www.linkedin.com/in/gabrielle-savary/" mediaIconColor="white" />
           <SocialMedia mediaName="github" mediaLink="https://github.com/Gabrielle-SAVARY?tab=repositories" mediaIconColor="white" />
         </div>
