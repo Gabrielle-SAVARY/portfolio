83ee642d5468b668906249a48cf49f2d12f23e1f
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-03-05 21:52:25 +0100

    Footer: add copyright and credit image homepage

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,13 +9,18 @@ function Footer() {
         <a href="/">
           <img className="footer__container__logo" src={portfolioLogo} alt="logo du portfolio avec initiales" />
         </a>
-        <div className="footer__container__ocial-media">
+        <div className="footer__container__social-media">
           <SocialMedia mediaName="linkedin" mediaLink="https://www.linkedin.com/in/gabrielle-savary/" mediaIconColor="white" />
           <SocialMedia mediaName="github" mediaLink="https://github.com/Gabrielle-SAVARY?tab=repositories" mediaIconColor="white" />
         </div>
       </div>
       <div className="footer__copyright">
         <p className="footer__copyright-text">© 2023 Gabrielle Savary</p>
+        <p className="footer__copyright-text">
+          Crédit éléments d&apos;image accueil -
+          {' '}
+          <a href="https://fr.freepik.com/vecteurs-libre/dessin-main-style-original_1322923.htm#fromView=search&page=1&position=12&uuid=e78ce822-6526-44f7-ae31-72606b4c941a" target="_blank" rel="noreferrer">Designed by Freepik.com</a>
+        </p>
       </div>
     </footer>
 
