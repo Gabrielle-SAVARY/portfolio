d8f09532cb18a48848c083c9e646178e7b9fc729
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2024-03-26 22:39:09 +0100

    About: update text

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,23 +8,60 @@ function About() {
   return (
     <section id="sectionAbout" className="section-about">
       <div className="about">
-        <div className="about__header">
-          <img className="about__header__img" src={portraitImage} alt="portrait de profil version animée d'une fille avec un caque audio" />
+        <div className="about__img-container">
+          <img className="about__img-container__img" src={portraitImage} alt="portrait de profil version animée d'une fille avec un caque audio" />
         </div>
-        <div className="about__container">
-          <h2 className="about__header__title">À propos</h2>
-          <p className="about__container__text">
-            <em className="font-accent__text"> Tout développeur &quot; senior&quot; a été un &quot;junior&quot; qui a pu monter en compétences et avoir de l'expérience en entreprise.</em>
-          </p>
-          <p className="about__container__text">
-            Je suis à la recherche d'une opportunité professionnelle afin de m'insérer dans une équipe dynamique que ce soit en alternance ou en CDI.
-          </p>
-          <p className="about__container__text">
-            Les compétences ne sont pas statiques, elles évoluent constamment. Bien qu'encore qu'à l'état brut, mes compétences, peuvent être façonnées et polies en entreprise.
-          </p>
-          <div className="about__container__links">
-            <div className="about__container__links__contact-link">
-              <img src={diamondImg} alt="dessin diamant" className="about__container__links__contact-link__img" />
+        <div className="about__infos-container">
+          <h2 className="about__infos-container__title">À propos</h2>
+          <div className="about__infos-container__paragraph">
+            <h3 className="about__infos-container__paragraph__title heading-gradient">Mon parcours</h3>
+            <p className="about__infos-container__text">
+              En bref, j&apos;ai décidé de
+              {' '}
+              <em className="font-accent__text">troquer l&apos;application code du travail pour coder des applications web</em>
+              .
+            </p>
+            <div className="about__infos-container__paragraph__timeline">
+              <h4 className="about__infos-container__paragraph__timeline-date heading-gradient">2015</h4>
+              <p className="about__infos-container__text">J&apos;ai découvert le développement web en autodidacte pendant mes études en ressources humaines.</p>
+            </div>
+            <div className="about__infos-container__paragraph__timeline">
+              <h4 className="about__infos-container__paragraph__timeline-date heading-gradient">Décembre 2022</h4>
+              <p className="about__infos-container__text">
+                Je décide de me reconvertir pour continuer se qui m&apos;avait passionnée.
+                <br />
+                Grâce à une formation de 6 mois chez O&apos;clock j'ai acquis un socle de compétences en développement javascript.
+              </p>
+            </div>
+            <div className="about__infos-container__paragraph__timeline">
+              <h4 className="about__infos-container__paragraph__timeline-date heading-gradient">Octobre 2023</h4>
+              <p className="about__infos-container__text">
+                <em className="font-accent__text">J&apos;obtiens mon titre de Développeur Web et Mobile</em>
+                {' '}
+                après être passée devant un jury de professionnels.
+              </p>
+            </div>
+          </div>
+          <div className="about__infos-container__paragraph">
+            <h3 className="about__infos-container__paragraph__title heading-gradient">Mon objectif en 2024</h3>
+            <p className="about__infos-container__text">
+              Je souhaite apporter mon dynamisme au sein d&apos;une équipe qui me permettra de continuer à évoluer.
+            </p>
+            <p className="about__infos-container__text">
+              Les compétences ne sont pas statiques, elles évoluent constamment.
+              <br />
+              Bien qu&apos;encore à l&apos;état brut,
+              {' '}
+              <em className="font-accent__text">mes compétences vont être façonnées et polies en entreprise</em>
+              .
+            </p>
+          </div>
+          <div className="about__infos-container__paragraph">
+            <h3 className="about__infos-container__paragraph__title-contact heading-gradient"> Si vous êtes intéréssé par mon profil, échangeons ensemble sur linkedin.</h3>
+          </div>
+          <div className="about__infos-container__links">
+            <div className="about__infos-container__links__contact-link">
+              <img src={diamondImg} alt="dessin diamant" className="about__infos-container__links__contact-link__img" />
               <MyLink
                 label="me contacter"
                 path="https://www.linkedin.com/in/gabrielle-savary/"
